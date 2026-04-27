b5bab24ce1eca948de36251680b09c6bb814f627
Firebase Studio <noreply@firebase.studio>
2026-04-27 19:41:22 +0000

    Initialized workspace with Firebase Studio

---
+++ src/bootstrap.js
@@ -0,0 +1,10 @@
+window["APP_TEMPLATE_BOOTSTRAP"] = {
+    firebase: {
+      apiKey: "AIzaSyAaWpq2KAAFdXrVka8qU4gjRV7zuOuDFQQ",
+      authDomain: "smart-web-builder-ai.firebaseapp.com",
+      projectId: "smart-web-builder-ai",
+      storageBucket: "smart-web-builder-ai.firebasestorage.app",
+      messagingSenderId: "432716522671",
+      appId: "1:432716522671:web:abc1dc8eed3a6e57a7605a",
+    }
+    };
